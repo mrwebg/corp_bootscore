b446245f2a14fcfe01596ec37bd47417d56ff32d
mrwebg <maarten@mrweb.nl>
2022-06-22 16:15:26 +0200

    Update featured-image-background.php

--- includes/featured-image-background.php
+++ includes/featured-image-background.php
@@ -1,6 +1,7 @@
 <?php
 function corp_featured_image_background() {
-  if ( is_page()){
+  //if ( is_page()){
+  if (is_front_page()) {
     global $post;
     $html = '<!-- featured-image-background -->';
     $featured_image_alt = $post->post_title;
