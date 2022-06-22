26e8db9d88a042c2b3729694574c0e9943bf6df1
mrwebg <maarten@mrweb.nl>
2022-06-22 16:40:29 +0200

    widget-contact-sidebar
    telefoon, email, maps link van bedrijfsgegevens ge-output op iedere pagina

--- includes/widget-contact-sidebar.php
+++ includes/widget-contact-sidebar.php
@@ -3,25 +3,24 @@ function corp_widget_contact_sidebar() {
   if ( is_page()){
     global $post;
     $html = '<!-- widget-contact-sidebar -->';
-    // $featured_image_alt = $post->post_title;
-    // $featured_thumbail_id = get_post_thumbnail_id($post->ID);
-    // if(!empty($featured_thumbail_id) && $featured_thumbail_id > 0){
-    //   $featured_image_source = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), "full")[0];
-    //   if(!empty($featured_image_source) && '' !== $featured_image_source){
-    //     $html .='
-    //     <figure class="featured-image-background">
-    //     <picture>
-    //     <img src="'.$featured_image_source.'" alt="'.$featured_image_alt.'">
-    //     </picture>
-    //     <noscript>
-    //     <picture>
-    //     <img src="'.$featured_image_source.'" alt="'.$featured_image_alt.'">
-    //     </picture>
-    //     </noscript>
-    //     </figure>
-    //     ';
-    //   }
-    // }
+    // BEDRIJFSGEGEVENS
+    $bedrijfsgegevens = get_option('bedrijfsgegevens');
+    if(is_array($bedrijfsgegevens) && count($bedrijfsgegevens) > 0 ){
+      $adres_telefoon = (array_key_exists('adres_telefoon', $bedrijfsgegevens))? $bedrijfsgegevens['adres_telefoon'] : '';
+      $adres_email = (array_key_exists('adres_email', $bedrijfsgegevens))? $bedrijfsgegevens['adres_email'] : '';
+      $maps_url = (array_key_exists('maps_url', $bedrijfsgegevens))? $bedrijfsgegevens['maps_url'] : '';
+      if(!empty($adres_telefoon) && '' !== $adres_telefoon && !empty($adres_email) && '' !== $adres_email && !empty($maps_url) && '' !== $maps_url){
+        $html .='
+        <div class="widget-contact-sidebar-wrapper">
+        <ul class="widget-contact-sidebar">
+        <li>'.$adres_telefoon.'</li>
+        <li>'.$adres_email.'</li>
+        <li>'.$maps_url.'</li>
+        </ul>
+        </div>
+        ';
+      }
+    }
     echo $html;
   }
 }
