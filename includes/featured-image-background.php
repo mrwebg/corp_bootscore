66da695ce2fd413142ed2cb273f09ddce1b71efb
mrwebg <maarten@mrweb.nl>
2022-06-22 15:48:32 +0200

    Update featured-image-background.php

--- includes/featured-image-background.php
+++ includes/featured-image-background.php
@@ -2,24 +2,27 @@
 function corp_featured_image_background() {
   if ( is_page()){
     global $post;
-    $featured_image_source = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), "full")[0];
-    $featured_image_alt = $post->post_title;
     $html = '<!-- featured-image-background -->';
-    if(!empty($featured_image_source) && '' !== $featured_image_source){
-      $html .='
-      <figure class="featured-image-background" style="display:none;">
+    $featured_image_alt = $post->post_title;
+    $featured_thumbail_id = get_post_thumbnail_id($post->ID);
+    if(!empty($featured_thumbail_id) && $featured_thumbail_id > 0){
+      $featured_image_source = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), "full")[0];
+      if(!empty($featured_image_source) && '' !== $featured_image_source){
+        $html .='
+        <figure class="featured-image-background" style="display:none;">
         <picture>
-          <img src="'.$featured_image_source.'" alt="'.$featured_image_alt.'">
+        <img src="'.$featured_image_source.'" alt="'.$featured_image_alt.'">
         </picture>
         <noscript>
-          <picture>
-            <img src="'.$featured_image_source.'" alt="'.$featured_image_alt.'">
-          </picture>
+        <picture>
+        <img src="'.$featured_image_source.'" alt="'.$featured_image_alt.'">
+        </picture>
         </noscript>
-      </figure>
-      ';
+        </figure>
+        ';
+      }
+      echo $html;
     }
-    echo $html;
   }
 }
 add_action( 'wp_footer', 'corp_featured_image_background' );
