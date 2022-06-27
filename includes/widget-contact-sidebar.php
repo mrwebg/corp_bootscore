d47dfd0d3facde10af611531db9682a2e8250483
mrwebg <maarten@mrweb.nl>
2022-06-27 15:20:48 +0200

    svg & contact-widget

--- includes/widget-contact-sidebar.php
+++ includes/widget-contact-sidebar.php
@@ -9,31 +9,26 @@ function corp_widget_contact_sidebar() {
     if(is_array($bedrijfsgegevens) && count($bedrijfsgegevens) > 0 ){
       $adres_telefoon = (array_key_exists('adres_telefoon', $bedrijfsgegevens))? $bedrijfsgegevens['adres_telefoon'] : '';
       $adres_email = (array_key_exists('adres_email', $bedrijfsgegevens))? $bedrijfsgegevens['adres_email'] : '';
+      $social_linkedin = (array_key_exists('social_linkedin', $bedrijfsgegevens))? $bedrijfsgegevens['social_linkedin'] : '';
       $maps_url = (array_key_exists('maps_url', $bedrijfsgegevens))? $bedrijfsgegevens['maps_url'] : '';
       if(!empty($adres_telefoon) && '' !== $adres_telefoon && !empty($adres_email) && '' !== $adres_email && !empty($maps_url) && '' !== $maps_url){
         ?>       
         <div class="widget-contact-sidebar-wrapper">
-          <ul class="widget-contact-sidebar" style="list-style:none;margin:0;">
+          <ul class="widget-contact-sidebar">
             <li>
-              <div class="d-flex flex-row mb-0">
-                <div class="p-2"><svg class="icon icon-phone"><use xlink:href="#icon-phone"></use></svg></div>
-                <div class="p-2"><?php echo $adres_telefoon;?></div>       
-              </div>
+              <svg class="icon icon-phone"><use xlink:href="#icon-phone"></use></svg><span><?php echo $adres_telefoon;?></span>  
+            </li>     
             <li>
-              <div class="d-flex flex-row mb-0">
-                <div class="p-2"><svg class="icon icon-email"><use xlink:href="#icon-email"></use></svg></div>
-                <div class="p-2"><?php echo $adres_email;?></div>       
-              </div>
+              <svg class="icon icon-email"><use xlink:href="#icon-email"></use></svg><span class="p-2"><?php echo $adres_email;?></span>       
             </li>
             <li>
-              <div class="d-flex flex-row mb-0">
-                <div class="p-2"><svg class="icon icon-location"><use xlink:href="#icon-location"></use></svg></div>
-                <div class="p-2"><?php echo $maps_url;?></div>       
-              </div>
+              <svg class="icon icon-linkedin"><use xlink:href="#icon-linkedin"></use></svg><span class="p-2"><?php echo $social_linkedin;?></span>       
+            </li>            
+            <li>
+              <svg class="icon icon-location"><use xlink:href="#icon-location"></use></svg><span class="p-2"><?php echo $maps_url;?></span>       
             </li>
           </ul>
         </div>
-        
 <?php
       }
     }
