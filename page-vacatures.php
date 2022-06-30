13c04f67b342b5ab44441d0c1edbf05dadf5312d
mrwebg <maarten@mrweb.nl>
2022-06-30 15:36:26 +0200

    Update page-vacatures.php

--- page-vacatures.php
+++ page-vacatures.php
@@ -5,46 +5,48 @@
 * @package corp_bootscore
 */
 get_header();?>
-<!-- cpt vacature
--->
-<?php
-$post_type = 'vacature';
-$post_args = array(
-  'post_type' => $post_type,
-  'post_status' => 'publish',
-  'posts_per_page' => -1,
-  'order' => 'ASC',
-  'orderby' => 'menu_order'
-);
-if (!is_admin()) {
-  if (function_exists('pll_is_translated_post_type') && false !== pll_is_translated_post_type($post_type)) {
-    $current_post_id = (isset($_GET['post'])) ? $_GET['post'] : '';
-    if (!empty($current_post_id) && function_exists('pll_get_post_language')) {
-      $current_post_language = pll_get_post_language($current_post_id);
-      if (false !== $current_post_language) {
-        $post_args['lang'] = $current_post_language;
-      }
-    }
-  }
-}
-$post_array = get_posts($post_args);
-?>
-<div id="content" class="site-content container py-5 mt-5">
+<div id="content" class="site-content">
   <div id="primary" class="content-area">
-    <div class="row">
-      <div class="">
-        <main id="main" class="site-main">
-          <header class="entry-header">
-            <?php the_post(); ?>
-            <!-- Title -->
-            <?php the_title('<h1>', '</h1>'); ?>
-            <!-- .entry-header -->
-          </header>
-          <div class="entry-content">
+    <main id="main" class="site-main">
+      <div class="container entry-content">        
+        <div class="row">
+          <div class="col-lg-6 parallax-col" data-scroll-speed="12">
+            <header class="entry-header">
+              <?php the_post(); ?>
+              <!-- Title -->
+              <?php the_title('<h1>', '</h1>'); ?>
+              <!-- .entry-header -->
+            </header>
             <!-- Content -->
             <?php the_content(); ?>
-            <?php if (is_array($post_array) && count($post_array) > 0): ?>
-              <div class="row row-cols-1 row-cols-md-4 g-4">
+
+            </div><!-- #col -->
+            <div class="col-6 parallax-col" data-scroll-speed="1">
+            <!-- VACATURES -->
+            <?php
+            $post_type = 'vacature';
+            $post_args = array(
+              'post_type' => $post_type,
+              'post_status' => 'publish',
+              'posts_per_page' => -1,
+              'order' => 'ASC',
+              'orderby' => 'menu_order'
+            );
+            if (!is_admin()) {
+              if (function_exists('pll_is_translated_post_type') && false !== pll_is_translated_post_type($post_type)) {
+                $current_post_id = (isset($_GET['post'])) ? $_GET['post'] : '';
+                if (!empty($current_post_id) && function_exists('pll_get_post_language')) {
+                  $current_post_language = pll_get_post_language($current_post_id);
+                  if (false !== $current_post_language) {
+                    $post_args['lang'] = $current_post_language;
+                  }
+                }
+              }
+            }
+            $post_array = get_posts($post_args);
+            ?>            
+            <?php if (is_array($post_array) && count($post_array) > 0){ ?>
+              <div class="row">
                 <?php
                 foreach ($post_array as $post):
                   setup_postdata($post);
@@ -60,12 +62,13 @@ $post_array = get_posts($post_args);
                   </div><!-- .col -->
                 <?php endforeach;?>
                 <?php wp_reset_postdata(); ?>
-              <?php endif; ?>
-              <!-- .entry-content -->
-            </div><!-- .row -->
-          </main><!-- #main -->
-        </div><!-- col -->
-      </div><!-- row -->
+              </div>
+              <?php } ?>
+              <!-- #VACATURES -->          
+            </div><!-- #col -->
+          </div><!-- #row -->
+        </div><!-- #container -->      
+      </main><!-- #main -->      
     </div><!-- #primary -->
   </div><!-- #content -->
   <?php
