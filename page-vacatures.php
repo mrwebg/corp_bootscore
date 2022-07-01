4fd785e92123c047a2f11eeb105411ebb809ad89
mrwebg <maarten@mrweb.nl>
2022-07-01 14:47:33 +0200

    Update page-vacatures.php

--- page-vacatures.php
+++ page-vacatures.php
@@ -52,7 +52,7 @@ get_header();?>
                   $url = get_the_permalink();              
                   ?>
                   <div class="corp-vacature">
-                    <h5 class="corp-vacature-title"><a href="<?php echo esc_url($url);?>"><?php the_title(); ?></a></h5><!-- .corp-vacature-title -->
+                    <h2 class="corp-vacature-title"><a href="<?php echo esc_url($url);?>"><?php the_title(); ?></a></h2><!-- .corp-vacature-title -->
                     <?php the_content(); ?>
                   </div><!-- /.corp-vacature -->                    
                 <?php endforeach;?>
@@ -82,7 +82,7 @@ get_header();?>
               } 
               if(!empty($post_title) && !empty($text_email) && !empty($permalink)){?>
                 <div class="wrapper-vacatures-rechter-kolom-contactpersoon"> 
-                  <h2><a href="<?php echo $permalink;?>"><?php echo $post_title; ?></a></h2>                          
+                  <h3><a href="<?php echo $permalink;?>"><?php echo $post_title; ?></a></h3>                          
                   <ul class="contactperson-icons">
                     <?php if(!empty($text_telefoon_formatted)) { ?>
                       <li>
