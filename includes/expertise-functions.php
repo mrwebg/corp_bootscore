3df61822eddb692001f3ec0dc168226ffd2c2cc1
mrwebg <maarten@mrweb.nl>
2022-07-06 14:44:33 +0200

    Update expertise-functions.php

--- includes/expertise-functions.php
+++ includes/expertise-functions.php
@@ -3,8 +3,14 @@
 if(!function_exists('mrweb_expertise_page_url')){
     function mrweb_expertise_page_url($expertise_term_id){
         $url = 'not found yet';
+        $lang = WPLANG;
         $post_type = 'page';
         $expertise_category = 'expertise';
-        return $url;
+        // get_posts
+        $args = array(
+            'numberposts' => -1,
+            'post_type'   => 'page'
+          );
+        return $lang;
     }
 }
