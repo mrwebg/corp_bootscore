eae74dac477cf16c73fd41efb2a5d02dab514aa3
mrwebg <maarten@mrweb.nl>
2022-07-06 15:55:04 +0200

    Update featured-image-background.php

--- includes/featured-image-background.php
+++ includes/featured-image-background.php
@@ -2,13 +2,14 @@
 function corp_featured_image_background() {
   global $post;
   if (! is_page_template( 'page-team.php' ) && 'mensen' !== $post->post_type  && 'vacature' !== $post->post_type || ! is_page_template( 'page-contact.php' )) {
-    $html = '<!-- .featured-image-background -->';
+    $html = '';
     $featured_image_alt = $post->post_title;
     $featured_thumbail_id = get_post_thumbnail_id($post->ID);
     if(!empty($featured_thumbail_id) && $featured_thumbail_id > 0){
       $featured_image_source = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), "full")[0];
       if(!empty($featured_image_source) && '' !== $featured_image_source){
         $html .='
+        <!-- .featured-image-background -->
         <figure class="featured-image-background">
         <picture>
         <img src="'.$featured_image_source.'" alt="'.$featured_image_alt.'">
