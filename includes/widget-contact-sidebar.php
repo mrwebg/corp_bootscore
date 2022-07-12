389bba3e5660e6f2006d6ffe4d7e276e6226fcd4
mrwebg <maarten@mrweb.nl>
2022-07-12 14:35:20 +0200

    tooltips in contact sidebar

--- includes/widget-contact-sidebar.php
+++ includes/widget-contact-sidebar.php
@@ -13,17 +13,20 @@ function corp_widget_contact_sidebar() {
       <!-- widget-contact-sidebar-wrapper --> 
       <div class="widget-contact-sidebar-wrapper">
         <ul class="widget-contact-sidebar">
+          <li class="d-none d-md-block"><!-- phone icon with tooltip on desktop -->
+            <a href="#" data-bs-toggle="tooltip" data-bs-original-title="<?php echo $adres_telefoon;?>"><svg class="icon icon-phone"><use xlink:href="#icon-phone"></use></svg></a> 
+          </li>  
+          <li class="d-block d-md-none"><!-- phone icon with tel: link on mobile -->
+            <a href="tel:<?php echo $adres_telefoon_formatted;?>" target="_blank"><svg class="icon icon-phone"><use xlink:href="#icon-phone"></use></svg></a>
+          </li>             
           <li>
-            <a href="tel:<?php echo $adres_telefoon_formatted;?>" target="_blank"><svg class="icon icon-phone"><use xlink:href="#icon-phone"></use></svg><span><?php echo $adres_telefoon;?></span></a> 
-          </li>     
-          <li>
-            <a href="mailto:<?php echo $adres_email;?>" target="_blank"><svg class="icon icon-email"><use xlink:href="#icon-email"></use></svg><span><?php echo $adres_email;?></span></a>      
+            <a href="mailto:<?php echo $adres_email;?>" data-bs-toggle="tooltip" data-bs-original-title="<?php echo $adres_email;?>" target="_blank"><svg class="icon icon-email"><use xlink:href="#icon-email"></use></svg><span><?php echo $adres_email;?></span></a>      
           </li>
           <li>
-            <a href="<?php echo $social_linkedin;?>" target="_blank"><svg class="icon icon-linkedin"><use xlink:href="#icon-linkedin"></use></svg><span><?php _e('Volg ons op Linkedin', 'corp_bootscore'); ?></span></a>       
+            <a href="<?php echo $social_linkedin;?>" data-bs-toggle="tooltip" data-bs-original-title="<?php echo esc_html_e('Volg ons op Linkedin', 'corp' ); ?>" target="_blank"><svg class="icon icon-linkedin"><use xlink:href="#icon-linkedin"></use></svg><span><?php esc_html_e('Volg ons op Linkedin', 'corp'); ?></span></a>       
           </li>            
           <li>
-            <a href="<?php echo $maps_url;?>" target="_blank"><svg class="icon icon-location"><use xlink:href="#icon-location"></use></svg><span><?php esc_html_e('Lokatie op Google Maps', 'corp'); ?></span></a>       
+            <a href="<?php echo $maps_url;?>" data-bs-toggle="tooltip" data-bs-original-title="<?php echo esc_html_e('Locatie op Google Maps', 'corp' ); ?>" target="_blank"><svg class="icon icon-location"><use xlink:href="#icon-location"></use></svg><span><?php esc_html_e('Lokatie op Google Maps', 'corp'); ?></span></a>       
           </li>                        
         </ul>
       </div>
--- js/custom.js
+++ js/custom.js
@@ -89,4 +89,10 @@ jQuery(function ($) {
     }
   })
 
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+
+
 }); // jQuery End
