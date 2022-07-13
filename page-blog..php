7be60d1556441c301e5f647f85eb546c3df1fdcd
mrwebg <maarten@mrweb.nl>
2022-07-13 16:04:27 +0200

    test blog paginas

---
+++ page-blog..php
@@ -0,0 +1,26 @@
+<?php
+/**
+* Template Name: Blog (alle berichten)
+* @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+* @package corp_bootscore
+*/
+get_header();?>
+<div id="content" class="site-content">
+  <div id="primary" class="content-area">
+    <main id="main" class="site-main">
+        <div class="container entry-content"> 
+            <div class="row">
+                <div class="col ">
+                    een bericht
+                </div>
+            </div>
+            <div class="row">
+                <div class="col col-md-2">
+                    zes berichten
+                </div>
+            </div>            
+        </div>           
+    </main><!-- /#main -->
+  </div><!-- /.primary -->
+</div><!-- /.content -->
+<?php get_footer();
