bd4167cc679a3aa3b7570d65359b79e4a055f411
mrwebg <maarten@mrweb.nl>
2022-07-15 09:42:58 +0200

    Update single.php

--- single.php
+++ single.php
@@ -5,65 +5,50 @@
 
 get_header();  ?>
 
-<div id="content" class="site-content container py-5 mt-4 SINGLEMRWEB">
+<div id="content" class="site-content SINGLEMRWEB">
   <div id="primary" class="content-area">
-
-    <!-- Hook to add something nice -->
-    <?php bs_after_primary(); ?>
-
-    <?php the_breadcrumb(); ?>
-
-    <div class="row">
-      <div class="col-md-8 col-xxl-9">
-
-        <main id="main" class="site-main">
-
-          <header class="entry-header">
-            <?php the_post(); ?>
-            <?php bootscore_category_badge(); ?>
-            <?php the_title('<h1>', '</h1>'); ?>
-            <p class="entry-meta">
-              <small class="text-muted">
-                <?php
-                bootscore_date();
-                _e(' by ', 'bootscore');
-                the_author_posts_link();
-                bootscore_comment_count();
-                ?>
-              </small>
-            </p>
+    <main id="main" class="site-main">
+      <div class="container entry-content">  
+      <div class="row">
+          <div class="col-6">
+            <header class="entry-header">
+              <?php the_post(); ?>
+              <h1><?php echo get_the_title(); ?></h1>
+              <?php bootscore_post_thumbnail(); ?>
+            </header>
+            <!-- thumbnail -->
             <?php bootscore_post_thumbnail(); ?>
-          </header>
-
-          <div class="entry-content">
-            <?php the_content(); ?>
-          </div>
-
-          <footer class="entry-footer clear-both">
-            <div class="mb-4">
-              <?php bootscore_tags(); ?>
+          </div><!-- /.col -->  
+          <div class="col-6">
+            <div class="entry-content">
+              <?php the_content(); ?>
             </div>
-            <nav aria-label="Page navigation example">
-              <ul class="pagination justify-content-center">
-                <li class="page-item">
-                  <?php previous_post_link('%link'); ?>
-                </li>
-                <li class="page-item">
-                  <?php next_post_link('%link'); ?>
-                </li>
-              </ul>
-            </nav>
-          </footer>
+            Contact optional
+          </div><!-- /.col -->            
+        </div><!-- /.row -->
+        
+        <div class="row">
+          <div class="col">        
+          <footer class="entry-footer clear-both">
+              <div class="mb-4">
+                <?php bootscore_tags(); ?>
+              </div>
+              <nav aria-label="Page navigation example">
+                <ul class="pagination justify-content-center">
+                  <li class="page-item">
+                    <?php previous_post_link('%link'); ?>
+                  </li>
+                  <li class="page-item">
+                    <?php next_post_link('%link'); ?>
+                  </li>
+                </ul>
+              </nav>
+            </footer>
+          </div><!-- /.col -->            
+        </div><!-- /.row -->
 
-          <?php comments_template(); ?>
-
-        </main> <!-- #main -->
-
-      </div><!-- col -->
-      <?php get_sidebar(); ?>
-    </div><!-- row -->
-
-  </div><!-- #primary -->
-</div><!-- #content -->
-
-<?php get_footer(); ?>
+      </div> <!-- /.container -->           
+    </main><!-- /#main -->
+  </div><!-- /#primary -->
+</div><!-- /#content -->
+<?php get_footer();
